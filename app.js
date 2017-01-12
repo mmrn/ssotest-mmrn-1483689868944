92d6ff199386e381e81d3b3fb3ef39a6f63194ee
mmrn <mmrn@jp.ibm.com>
2017-01-12 17:00:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -77,3 +77,22 @@ app.get(‘/auth/sso/callback’, passport.authenticate(‘openidconnect’, {
   //……
   });
 });
+
+//引野さんのコードを追加
+//catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+//error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
